7999f922419e6fa0980ba80179215e06f3269572
robert-devs <mainrobert04@gmail.com>
2023-03-03 17:16:56 +0300

    adding the gallery container

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React from 'react'
+import { SubHeading } from '../../components'
+import { images, data } from '../../constants'
 
 const FindUs = () => (
-  <div>
-    FindUs
+  <div className="app__bg app__wrapper section__padding" id="contact">
+    <div className="app__wrapper_info"></div>
+    <div className="app__wrapper_img">
+      <img src={images.findus} alt="findUs" />
+    </div>
   </div>
-);
+)
 
-export default FindUs;
+export default FindUs
